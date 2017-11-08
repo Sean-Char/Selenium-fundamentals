9a07a69ac1a35ec627c50cf8993736810bd04799
Sean Charoutanh <scharoutanh@gmail.com>
2017-11-08 15:18:13 -0600

    initial environment set up

---
+++ SetupEnvironment/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver(); // create instance of driver
+
+        driver.Navigate().GoToUrl("http://testing.todvachev.com");
+
+        Thread.Sleep(3000); // keep page on for 3 seconds
+
+        driver.Quit();
+    }
+}
+
